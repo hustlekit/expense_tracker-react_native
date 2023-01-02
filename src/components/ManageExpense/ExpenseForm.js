e49f93ad42b180a5d631b1216358c2f723a5bd00
Christopher Rush <ceo@christopherhustle.com>
2023-01-02 22:00:36 +0100

    ADD expense adding and updating in generic way

--- src/components/ManageExpense/ExpenseForm.js
+++ src/components/ManageExpense/ExpenseForm.js
@@ -1,10 +1,39 @@
 import { StyleSheet, Text, View } from 'react-native';
 import Input from './Input';
 import { GlobalStyles } from '../../constants/styles';
+import { useState } from 'react';
+import Button from '../ui/Button';
+import { getFormattedDate } from '../../util/date';
 
-const ExpenseForm = () => {
-  const amountChangeHandler = () => {
+const ExpenseForm = ({ onCancel, onSubmit, submitButtonLabel, defaultValues }) => {
+  const [ inputValues, setInputValues ] = useState({
+    amount: defaultValues ? defaultValues.amount.toString() : '',
+    date: defaultValues ? getFormattedDate(defaultValues.date) : '',
+    description: defaultValues ? defaultValues.description : '',
+  });
   
+  const inputChangeHandler = (inputIdentifier, value) => {
+    setInputValues((currInputValues) => {
+      return {
+        ...currInputValues,
+        [inputIdentifier]: value,
+      };
+    });
+  };
+  
+  const submitHandler = () => {
+    console.log(inputValues)
+    console.log(inputValues.amount);
+    console.log(typeof inputValues.amount);
+    console.log(Number(inputValues.amount));
+    console.log(typeof Number(inputValues.amount));
+    const expenseData = {
+      amount: Number(inputValues.amount),
+      date: new Date(inputValues.date),
+      description: inputValues.description,
+    };
+    
+    onSubmit(expenseData);
   };
   
   return (
@@ -16,7 +45,8 @@ const ExpenseForm = () => {
           style={ styles.rowInput }
           textInputConfig={ {
             keyboardType: 'decimal-pad',
-            onChangeText: amountChangeHandler,
+            onChangeText: (value) => inputChangeHandler('amount', value),
+            value: inputValues.amount,
           } }
         />
         <Input
@@ -25,8 +55,8 @@ const ExpenseForm = () => {
           textInputConfig={ {
             placeholder: 'YYYY-MM-DD',
             maxLength: 10,
-            onChangeText: () => {
-            },
+            onChangeText: (value) => inputChangeHandler('date', value),
+            value: inputValues.date,
           } }
         />
       </View>
@@ -34,8 +64,14 @@ const ExpenseForm = () => {
         label={ 'Description' }
         textInputConfig={ {
           multiline: true,
+          onChangeText: (value) => inputChangeHandler('description', value),
+          value: inputValues.description,
         } }
       />
+      <View style={ styles.buttonContainer }>
+        <Button style={ styles.button } mode={ 'flat' } onPress={ onCancel }>Cancel</Button>
+        <Button style={ styles.button } onPress={ submitHandler }>{ submitButtonLabel }</Button>
+      </View>
     </View>
   );
 };
@@ -59,5 +95,14 @@ const styles = StyleSheet.create({
   },
   rowInput: {
     flex: 1,
+  },
+  buttonContainer: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  button: {
+    minWidth: 120,
+    marginHorizontal: 8,
   },
 });
--- src/screens/ManageExpense.js
+++ src/screens/ManageExpense.js
@@ -12,6 +12,8 @@ const ManageExpense = ({ route, navigation }) => {
   const editedExpenseId = route.params?.expenseId;
   const isEditing = !!editedExpenseId;
   
+  const selectedExpense = expensesCtx.expenses.find(expense => expense.id === editedExpenseId);
+  
   useLayoutEffect(() => {
     navigation.setOptions({
       title: isEditing ? 'Edit Expense' : 'Add Expense',
@@ -27,32 +29,24 @@ const ManageExpense = ({ route, navigation }) => {
     navigation.goBack();
   };
   
-  const confirmHandler = () => {
+  const confirmHandler = (expenseData) => {
     if ( isEditing ) {
       expensesCtx.updateExpense(
-        editedExpenseId,
-        {
-          description: 'Test!!!',
-          amount: 29.99,
-          date: new Date('2022-12-27'),
-        });
+        editedExpenseId, expenseData);
     } else {
-      expensesCtx.addExpense({
-        description: 'Test',
-        amount: 19.99,
-        date: new Date('2022-12-28'),
-      });
+      expensesCtx.addExpense(expenseData);
     }
     navigation.goBack();
   };
   
   return (
     <View style={ styles.container }>
-      <ExpenseForm />
-      <View style={ styles.buttonContainer }>
-        <Button style={ styles.button } mode={ 'flat' } onPress={ cancelHandler }>Cancel</Button>
-        <Button style={ styles.button } onPress={ confirmHandler }>{ isEditing ? 'Update' : 'Add' }</Button>
-      </View>
+      <ExpenseForm
+        onCancel={ cancelHandler }
+        onSubmit={ confirmHandler }
+        submitButtonLabel={ isEditing ? 'Update' : 'Add' }
+        defaultValues={ selectedExpense }
+      />
       { isEditing && (
         <View style={ styles.deleteContainer }>
           <IconButton icon={ 'trash' }
@@ -73,15 +67,6 @@ const styles = StyleSheet.create({
     flex: 1,
     padding: 24,
     backgroundColor: GlobalStyles.colors.khakiWeb,
-  },
-  buttonContainer: {
-    flexDirection: 'row',
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  button: {
-    minWidth: 120,
-    marginHorizontal: 8,
   },
   deleteContainer: {
     marginTop: 16,
